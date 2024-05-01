7b3efff7d9831f7afece22089f0d585281fce145
Muhammet Taha KIRKGÖZ <m.taha.kirkgoz@gmail.com>
2024-05-01 12:02:40 +0300

    EntityFrameworkCore.SqlServer ve EntityFrameworkCore.Tools uzantıları indirildi ve ApplicatinDbContext ile Database yapısı oluşturuldu.

---
+++ ChatApplicationServer/Context/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ChatApplicationServer.Context;
+
+public sealed class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}

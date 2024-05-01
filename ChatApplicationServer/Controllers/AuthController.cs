d1beeea3159f9ae6e8994769e479ebe98042a2b4
Muhammet Taha KIRKGÖZ <m.taha.kirkgoz@gmail.com>
2024-05-01 12:27:41 +0300

    Dosya işlemleri için GenericFileService kütüphanesi dahil edildi ve kullanıcı kayıt işlemleri için gerekli api isteği oluşturuldu.

---
+++ ChatApplicationServer/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using ChatApplicationServer.Context;
+using ChatApplicationServer.Dtos;
+using ChatApplicationServer.Models;
+using GenericFileService.Files;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatApplicationServer.Controllers;
+[Route("api/[controller]/[action]")]
+[ApiController]
+
+public sealed class AuthController(
+    ApplicationDbContext context) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterDto request, CancellationToken cancellationToken)
+    {
+        // IActionResult == Result yani geri dönüş tipi belirtmek için kullanılır (Json - Status - Redirect)
+        // CancellationToke == Bir görevin veya işlemin iptal edilmesine izin verir.
+
+        bool isNameExists = await context.Users.AnyAsync(p => p.Name == request.Name, cancellationToken);
+
+        if (isNameExists)
+            return BadRequest(new { Message = "Bu kullanıcı adı daha önce kullanılmış!" });
+
+        string avatar = FileService.FileSaveToServer(request.File, "wwwroot/avatar/");
+
+        User user = new User()
+        {
+            Name = request.Name,
+            Avatar = avatar,
+        };
+
+        await context.AddAsync(user, cancellationToken);
+        await context.SaveChangesAsync();
+        
+        return NoContent();
+    }
+}
---
+++ ChatApplicationServer/Dtos/RegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatApplicationServer.Dtos;
+
+public sealed record RegisterDto(
+    string Name,
+    IFormFile File
+    ); 
+
+// Sealed == Bir sınıfın miras alınamayacağı veya yeniden türetilemeyeceği anlamına gelir.
+// record == Basit veri kümelerini temsil etmek için kullanılır.
